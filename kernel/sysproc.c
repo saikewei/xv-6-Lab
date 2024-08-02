51a51bc01c1bf5c9b15d5f38f5cd0fbd4b6fc96f
huang yicheng <huangyicheng0302@qq.com>
2024-08-02 13:34:38 +0800

    实现alarm

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -98,3 +98,29 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64 sys_sigalarm(void)
+{
+  int ticks;
+  uint64 handler;
+
+  argint(0, &ticks);
+  argaddr(1, &handler);
+
+  struct proc *p = myproc();
+  p->ticks = ticks;
+  p->handler = handler;
+  p->ticks_cnt = 0;
+
+  return 0;
+}
+
+uint64 sys_sigreturn(void)
+{
+  struct proc *p = myproc();
+
+  *(p->trapframe) = p->tick_trapframe;
+
+  p->handler_executing = 0;
+  return 0;
+}
